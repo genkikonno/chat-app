f0e6660e991fa39384a84eff84fa4a3a7b8b59ff
genkikonno <98734420+genkikonno@users.noreply.github.com>
2022-02-08 12:57:37 +0900

    各モデルのテスト

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< Updated upstream
 ActiveRecord::Schema.define(version: 2022_02_07_090152) do
-=======
-ActiveRecord::Schema.define(version: 2022_02_07_083447) do
->>>>>>> Stashed changes
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
